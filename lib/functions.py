1f401529b6983584a66e1083467adfc7abbaba5a
Elias Bosire Ayunga <bosire.elias@gmail.com>
2025-03-06 10:49:04 +0300

    First commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    name = "Guido"
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, " + name + "!")
 
 def add(num1, num2):
-    pass
+    num1 = 45
+    num2 = 55
+    return num1 + num2  
+
+result = add(10, 20)  
+print(result) 
+
 
 def halve(number):
-    pass
+    return number / 2  
+
+print(halve(100))  
+print(halve(99))   
+
